685c16cd95abccacc348ab3b755f93d07a72c5dd
Angelo Fausti <afausti@lsst.org>
2017-07-20 13:00:03 -0700

    Make deployment work for GKE and minikube
    - On minikube we dont have an external IP address and the port
    to connect to the service is not 443.

--- kubernetes/replace.sh
+++ kubernetes/replace.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-HOST=$(kubectl describe services kubernetes | grep Endpoints | cut -f3 | cut -d: -f1)
-PORT=$(kubectl describe services squash-bokeh | grep NodePort | cut -f4 | cut -d/ -f1)
+
+HOST=$(kubectl describe services squash-bokeh | grep Ingress | cut -f4)
+PORT=443
+
+if test "$HOST" == ""; then
+    echo "It looks like you are running on Minikube..."
+    # minikube does not provide an external IP address
+    HOST=$(kubectl describe services kubernetes | grep Endpoints | cut -f3 | cut -d: -f1)
+    PORT=$(kubectl describe services squash-bokeh | grep NodePort | cut -f4 | cut -d/ -f1)
+fi
+
+echo "Service address: $HOST:$PORT"
 sed -e "
 s/{{ TAG }}/${TAG}/
 s/{{ HOST }}/${HOST}/
 s/{{ PORT }}/\"${PORT}\"/
-" $1
+" $1 > $2

32ac353fa6fe34e8f5772ebba8eab0e5344cf93b
Angelo Fausti <afausti@lsst.org>
2017-07-24 12:02:46 -0700

    On GKE wait for the service to become available

--- kubernetes/replace.sh
+++ kubernetes/replace.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
-HOST=$(kubectl describe services squash-bokeh | grep Ingress | cut -f4)
-PORT=443
+usage(){
+	echo "Usage: $0 <template configuration> <configuration>"
+	exit 1
+}
 
-if test "$HOST" == ""; then
-    echo "It looks like you are running on Minikube..."
-    # minikube does not provide an external IP address
-    HOST=$(kubectl describe services kubernetes | grep Endpoints | cut -f3 | cut -d: -f1)
-    PORT=$(kubectl describe services squash-bokeh | grep NodePort | cut -f4 | cut -d/ -f1)
+if [ "$1" == "" ] || [ "$2" == "" ]; then
+    usage
+fi
+
+# values returned if the service is not ready
+HOST=""
+PORT=""
+
+# on minikube we don't have a load balancer ingress IP
+if [ "$MINIKUBE" == "true" ]; then
+    HOST=$(kubectl get endpoints kubernetes -o jsonpath --template='{.subsets[0].addresses[0].ip}')
+    PORT=$(kubectl get services squash-bokeh -o jsonpath --template='{.spec.ports[0].nodePort}')
+else
+    # on GKE
+    WAIT_TIME=5
+    while [ "$HOST" == "" ] && [ "$WAIT_TIME" -le 10 ]; do
+        echo "Waiting for the service to become available..."
+        sleep $(( WAIT_TIME++ ))
+        HOST=$(kubectl get service squash-bokeh -o jsonpath --template='{.status.loadBalancer.ingress[0].ip}')
+    done
+    PORT=443
+fi
+
+if [ "$HOST" == "<no value>" ] || [ "$PORT" == "<no value>" ]; then
+    echo "Service is not ready..."
+    echo "If you are deploying to a minikube local cluster, make sure you set MINIKUBE=true."
+    exit 1
 fi
 
 echo "Service address: $HOST:$PORT"
+
 sed -e "
 s/{{ TAG }}/${TAG}/
 s/{{ HOST }}/${HOST}/

bf77c5d0b1f46065496fdad07d3023c4b969c338
Michael Reuter <mareuternh@gmail.com>
2018-07-20 11:30:43 -0700

    Adding comments to reset callback attempts.

--- app/sysver/sysver_interactions.py
+++ app/sysver/sysver_interactions.py
@@ -1,5 +1,5 @@
-#from bokeh import events
-#from bokeh.models import CustomJS, ResetTool
+from bokeh import events
+from bokeh.models import CustomJS, ResetTool
 import numpy as np
 
 from sysver_layout import Layout
@@ -14,15 +14,16 @@ class Interactions(Layout):
         super().__init__()
 
         self.pcircle.data_source.on_change('selected', self.calculate_avg_temp)
-        #callback = CustomJS(args={}, code="""console.log('Got Here!')""")
-        #self.plot.js_on_event('reset', callback)
+        callback = CustomJS(args={}, code="""console.log('Got Here!')""")
+
+        # Uncomment these three lines to create a new reset button for the tool bar.
+        # You'll need to remove the reset button from sysver_layout:49
         #reset_tool = ResetTool()
         #reset_tool.js_event_callbacks = {'reset': [callback]}
         #self.plot.add_tools(reset_tool)
 
-    #def reset_avg_temp(self, event):
-    #    print("A:", event)
-    #    self.update_ave_temp()
+        # Uncomment this line to just apply the callback. No need to fix the tool bar.
+        #self.plot.js_on_event('reset', callback)
 
     def calculate_avg_temp(self, attr, old, new):
 

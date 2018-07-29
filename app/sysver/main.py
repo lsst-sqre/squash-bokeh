3c8dc1d885118a3b2da89cf83dba124730d62106
Angelo Fausti <afausti@lsst.org>
2018-07-28 22:53:50 -0700

    CustomJS was not added to the bokeh document
    - The CustomJS element defined in the interactions class also needs to be added
    to the bokeh document. Moved the make_layout() method to main.py
    so that it is executed at the end.

--- app/sysver/main.py
+++ app/sysver/main.py
@@ -6,6 +6,7 @@ class SystemVerification(Interactions):
     def __init__(self, title):
         super().__init__()
         self.set_title(title)
+        self.make_layout()
 
 
 SystemVerification(title="System Verification App - LSST SQuaSH")

07f78cff6ec411925a1f195b0d8723186903d16c
danielsf <scott.f.daniel@gmail.com>
2018-09-05 10:39:25 -0700

    do scatter plot based on magnitude

--- app/source_ct/main.py
+++ app/source_ct/main.py
@@ -1,13 +1,14 @@
 import os
 import sys
+import numpy as np
 # This is needed to import the api_helper module
 BASE_DIR = os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))
 )
 sys.path.append(os.path.join(BASE_DIR, 'monitor'))
 
-from bokeh.models import ColumnDataSource
-from bokeh.models import Plot
+from bokeh.models import ColumnDataSource, LinearColorMapper
+from bokeh.plotting import figure
 from bokeh.models.markers import Circle
 from bokeh.models import BasicTicker, NumeralTickFormatter
 from bokeh.models import LinearAxis, DataRange1d
@@ -31,12 +32,26 @@ class SourceCtMetric(BaseApp):
         print('\ndata stats')
         for kk in data.keys():
             print(kk)
-        data = ColumnDataSource({'ra':data['ra_rad']['value'],
-                                 'dec':data['dec_rad']['value']})
+        mag_arr = -2.5*np.log10(data['flux']['value'])
+        column_data = ColumnDataSource({'ra':data['ra_rad']['value'],
+                                        'dec':data['dec_rad']['value'],
+                                        'flux':data['flux']['value'],
+                                        'mag':mag_arr,
+                                        'flux_sigma':data['flux_sigma']['value']})
 
-        plot = Plot()
-        sources = Circle(x='ra', y='dec', line_color=None, size=5)
-        plot.add_glyph(data, sources)
+
+
+        color_mapper = LinearColorMapper(palette='Magma256',
+                                         low=min(mag_arr),
+                                         high=max(mag_arr))
+
+
+        plot = figure()
+        plot.circle(x='ra', y='dec', source=column_data, size=5,
+                    color={'field':'mag', 'transform':color_mapper})
+
+        #plot.legend.location = "top_right"
+
         ra_axis = LinearAxis(formatter=NumeralTickFormatter(format="0.00"),
                              ticker=BasicTicker(desired_num_ticks=4),
                              minor_tick_line_width=2,
